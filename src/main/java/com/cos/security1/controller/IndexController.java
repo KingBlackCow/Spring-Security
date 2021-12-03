6b53d32edda88b9c5b1576c548cc01c7872a91ab
KingBlackCow <sgs1159@naver.com>
2021-12-03 22:26:21 +0900

    add : EnableGlobalMethodSecurity @secure, @preAuthorize 하는법

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -4,6 +4,9 @@ package com.cos.security1.controller;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,7 +45,7 @@ public class IndexController {
         return "manager";
     }
 
-    //스프링 시큐리티가 낚아채버림 - SecurityConfig파일 생성후 작동안함
+    //스프링 시큐리티가 낚아채버림 - SecurityConfig 파일 생성후 작동안함
     @GetMapping("/loginForm")
     public String loginForm(){
         return "loginForm";
@@ -62,5 +65,20 @@ public class IndexController {
         user.setPassword(encPassword);
         userRepository.save(user);
         return "redirect:/loginForm";
+    }
+
+    //특정한구역에 권한1개만, 글로벌은 securityconfig안에 anymatcher로
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info(){
+        return "개인정보";
+    }
+
+    //특정한구역에 권한2개이상일때, 글로벌은 securityconfig안에 anymatcher로
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") //data메서드 실행되기 직전에 실행
+    //@PostAuthorize() 함수가 끝난뒤에 실행
+    @GetMapping("/data")
+    public @ResponseBody String data(){
+        return "데이터정보";
     }
 }
